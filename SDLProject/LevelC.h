5a791b6954588865f385a53464ebacbccc329c43
Yanka Sikder <ys4780@nyu.edu>
2024-11-23 00:33:28 -0500

    set up 3 levels

---
+++ SDLProject/LevelC.h
@@ -0,0 +1,12 @@
+#include "Scene.h"
+
+class LevelC : public Scene {
+public:
+    int ENEMY_COUNT = 1;
+    
+    ~LevelC();
+    
+    void initialise() override;
+    void update(float delta_time) override;
+    void render(ShaderProgram *program) override;
+};
