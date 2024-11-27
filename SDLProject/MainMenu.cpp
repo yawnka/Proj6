8058510bb60a03a1878d0b5f27b3982d456a9fb2
Yanka Sikder <ys4780@nyu.edu>
2024-11-26 21:33:00 -0500

    changing header deadline for all the files

--- SDLProject/MainMenu.cpp
+++ SDLProject/MainMenu.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Yanka Sikder
 * Assignment: Platformer
-* Date due: 2023-12-02, 11:59pm
+* Date due: 2023-11-26, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
--- SDLProject/Map.cpp
+++ SDLProject/Map.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: Yanka Sikder
+* Assignment: Platformer
+* Date due: 2023-11-26, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #include "Map.h"
 
 Map::Map(int width, int height, unsigned int *level_data, GLuint texture_id, float tile_size, int tile_count_x, int tile_count_y) : 
